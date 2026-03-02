2ae8c8fc370e28d8ddd514df9325dd49ee2bfefa
Iona Xia <ionaxia@stanford.edu>
2026-03-01 20:56:36 -0800

    fix makefile and pi_bridge

--- project/pi-side/pi_bridge.c
+++ project/pi-side/pi_bridge.c
@@ -1,10 +1,8 @@
-#include "pi-bridge.h"
-#include "uart.h"
+#include "pi_bridge.h"
+#include "../../libpi/rpi.h"
 
 #define SERIAL_PORT "/dev/cu.SLAB_USBtoUART"
 #define BAUDRATE B115200
-
-struct termios old_t;
 
 struct coords p_coords = {0, 0, 0};
 
@@ -18,34 +16,54 @@ coords do_move(char c) {
         p_coords.x += 1;
     } else if (c == 'd') {
         p_coords.y -= 1;
-    } else {
-        panic("c wasn't a valid move c=%s", c);
     }
     return p_coords;
  }
 
-int main() {
+ void uart_put_int(int val) {
+    char buf[11];
+    int i = 10;
+    buf[i] = '\0';
+    i -= 1;
+
+    if (val < 0) {
+        uart_put8('-');
+        val = -val;
+    }
+
+    if (val == 0) buf[i] = '0';
+    i -= 1;
+    while (val > 0) {
+        buf[i] = '0' + (val % 10);
+        i -= 1;
+        val /= 10;
+    }
+    i++;
+    for (; buf[i] != '\0'; i++) {
+        uart_put8(buf[i]);
+    }
+}
+
+void notmain() {
     uart_init();
-    char c;
     while (1) {
         if (uart_has_data()) {
-            c = (char) uart_get8();
+            char c = (char) uart_get8();
 
             // dummy function, later will be updating pi side state
-            struct new_coord = do_move(c);
-            for (char ch : "player") {
-                uart_put8((uint8_t) ch);
+            struct coords new_coord = do_move(c);
+            
+            const char *str = "player";
+            for (size_t i = 0; str[i] != '\0'; i++) {
+                uart_put8((uint8_t) str[i]);
             }
-            uart_put8(" ");
-            uart_put8(new_coord.x);
-            uart_put8(" ");
-            uart_put8(new_coord.y);
-            uart_put8(" ");
-            uart_put8(new_coord.z);
-            uart_put8("\n");
-            printk("moved player to %d %d %d", new_coord.x, new_coord.y, new_coord.z);
+            uart_put8(' ');
+            uart_put_int(new_coord.x);
+            uart_put8(' ');
+            uart_put_int(new_coord.y);
+            uart_put8(' ');
+            uart_put_int(new_coord.z);
+            uart_put8('\n');
         }
     }
-    close(fd);
-    return 0;
 }
--- project/pi-side/pi_bridge.h
+++ project/pi-side/pi_bridge.h
@@ -2,12 +2,10 @@
 #ifndef PI_BRIDGE_H
 #define PI_BRIDGE_H
 
-#include <stdint.h>
-
 typedef struct coords {                                    
-    uint32_t  x;
-    uint32_t y;
-    uint32_t z;                    
+    int x;
+    int y;
+    int z;                    
 } coords; // x y z coordinates
 
 coords do_move(char c);
