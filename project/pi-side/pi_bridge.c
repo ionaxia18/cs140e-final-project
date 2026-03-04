514929d162907b60948b03c0d2d7bd6061b786ce
Erika Li <erikaecl@stanford.edu>
2026-03-04 06:15:05 -0800

    link do_move with world state and uart

--- project/pi-side/pi_bridge.c
+++ project/pi-side/pi_bridge.c
@@ -1,6 +1,10 @@
 #include "pi_bridge.h"
 #include "../../libpi/rpi.h"
 #include "../constants.h"
+#include "../world-state/world.h"
+#include "../world-state/world-gen.h"
+#include "../world-state/pending.h"
+#include "../world-state/hashtable.h"
 
 #define BAUDRATE B115200
 
@@ -32,25 +36,32 @@ void uart_put_int(int val) {
 }
 
 // takes in a character move, will move the player on pi side and return new coordinates
-void do_move(char c) {
+void do_move(world_t* w, char c) {
+    world_pos_t new_coord = w->player_pos;
     if (c == 'w') {
-        p_coords.x += 1;
+        new_coord.x += 1;
     } else if (c == 'a') {
-        p_coords.y -= 1;
+        new_coord.y -= 1;
     } else if (c == 's') {
-        p_coords.x += 1;
+        new_coord.x += 1;
     } else if (c == 'd') {
-        p_coords.y -= 1;
+        new_coord.y -= 1;
     }
+
+    if (!world_pos_is_valid(new_coord)) {
+        panic("invalid move to position %d %d %d", new_coord.x, new_coord.y, new_coord.z);
+    } 
+    
+    w->player_pos = new_coord;
 
     const char *str = "PLAYER";
     for (size_t i = 0; str[i] != '\0'; i++) {
         uart_put8((uint8_t) str[i]);
     }
     uart_put8(' ');
-    uart_put_int(p_coords.x);
-    uart_put_int(p_coords.y);
-    uart_put_int(p_coords.z);
+    uart_put_int(new_coord.x);
+    uart_put_int(new_coord.y);
+    uart_put_int(new_coord.z);
     uart_put8('\n');
  }
 
@@ -79,12 +90,30 @@ void change_block(char c) {
 
 
 void notmain() {
+    // initialize world seed
+    world_info_t info = {
+        .seed = 1,
+        .min = (world_pos_t){0, -60, 0},
+        .max = (world_pos_t){16, -44, 16},
+        .edits_cap = 4096,
+        .pending_cap = 4096,
+    };
+
+    world_t* w = world_create(&info);
+
+
+    if (!w) {
+        panic("Failed to create world");
+    }
+
+    // begin pulling from uart to update world
     uart_init();
+    trace("Comparison test passed\n");
     while (1) {
         if (uart_has_data()) {
             char c = (char) uart_get8();
             if (c == 'w' || c == 'a' || c == 's' || c == 'd') {
-                do_move(c);
+                do_move(w, c);
             }
             else if (c == 'p' || c == 'r') {
                 change_block(c);
