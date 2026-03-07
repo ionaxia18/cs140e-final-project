c33cc85406f23b1c63e4be40f6307515c492587a
erikae-cl <107904103+erikae-cl@users.noreply.github.com>
2026-03-06 17:32:25 -0800

    Delete libpi/objs directory

--- libpi/objs/cache-support.d
+++
@@ -1,7 +0,0 @@
-objs/cache-support.o: staff-src/cache-support.S \
- /Users/clarawang/Documents/cs140e-26win//libpi/include/rpi-asm.h \
- /Users/clarawang/Documents/cs140e-26win//libpi/include/rpi-constants.h
-
-/Users/clarawang/Documents/cs140e-26win//libpi/include/rpi-asm.h:
-
-/Users/clarawang/Documents/cs140e-26win//libpi/include/rpi-constants.h:
