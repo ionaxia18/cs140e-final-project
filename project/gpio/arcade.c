774caf0c0318db09c80ada2d3c60ba7984e37898
Erika Li <erika1a32s@gmail.com>
2026-03-11 16:41:47 -0700

    scale motion to half block

--- project/gpio/arcade.c
+++ project/gpio/arcade.c
@@ -14,18 +14,22 @@ pos_t arcade_read() {
     // dev_barrier();
     // trace("R=%d L=%d U=%d D=%d\n",gpio_read(PIN_RIGHT),gpio_read(PIN_LEFT),gpio_read(PIN_UP),gpio_read(PIN_DOWN));
     pos_t displacement = {0, 0, 0};
+    // left
     if (!gpio_read(PIN_LEFT)) {
-        displacement.x += 1;
+        displacement.x += -0.5;
     }
+    // right
     if (!gpio_read(PIN_RIGHT)) {
-        displacement.x += -1;
+        displacement.x += 0.5;
     }
 
+    // forward
     if (!gpio_read(PIN_UP)) {
-        displacement.z += 1;
+        displacement.z += 0.5;
     }
+    // backward
     if (!gpio_read(PIN_DOWN)) {
-        displacement.z += -1;
+        displacement.z += -0.5;
     }
     dev_barrier();
     return displacement;
