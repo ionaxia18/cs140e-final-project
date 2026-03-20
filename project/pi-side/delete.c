a58526044071c176554a500ef1197b6eebc52a6a
Erika Li <erika1a32s@gmail.com>
2026-03-19 23:07:33 -0700

    always delete 012 boot

--- project/pi-side/delete.c
+++ project/pi-side/delete.c
@@ -12,11 +12,22 @@ void notmain(void) {
     pi_dirent_t root;
     fat32_fs_t fs = initialize_fs(&root);
 
-    int delete = delete_boot_file(0, &root, &fs);
-    int deleted = delete_boot_file(2, &root, &fs);
+    int deleted = delete_boot_file(0, &root, &fs);
+    int deleted1 = delete_boot_file(1, &root, &fs);
+    int deleted2 = delete_boot_file(2, &root, &fs);
     if (deleted) {
-        printk("Deleted SERVER2.BIN\n");
+        printk("Deleted SERVER0.bin\n");
     } else {
-        printk("SERVER2.BIN not found or already deleted\n");
+        printk("SERVER0.bin not found or already deleted\n");
+    }
+    if (deleted1) {
+        printk("Deleted SERVER1.bin\n");
+    } else {
+        printk("SERVER1.bin not found or already deleted\n");
+    }
+        if (deleted2) {
+        printk("Deleted SERVER2.bin\n");
+    } else {
+        printk("SERVER2.bin not found or already deleted\n");
     }
 }
